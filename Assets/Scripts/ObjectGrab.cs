650d5aeb21add9aef35d8bd3c74b58d34d72010f
Huthaifa Mohammad <hmohamma@macalester.edu>
2024-11-25 11:23:11 -0600

    GoGoTechnique  Update
    Implemented Grabbing technique

---
+++ Assets/Scripts/ObjectGrab.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ObjectGrab : MonoBehaviour
+{
+    public GameObject collidingObject;
+    public GameObject objectInHand;
+    public InputActionAsset actions;
+    private InputAction grabAction;
+
+    void Awake()
+    {
+        // Updated to use XRI Default Input Actions
+        grabAction = actions.FindActionMap("XRI RightHand").FindAction("Select");
+        grabAction.performed += GrabObject;
+        grabAction.canceled += ReleaseObject;
+        Debug.Log("Grab action setup complete");
+    }
+
+    public void OnEnable()
+    {
+        grabAction.Enable();
+        Debug.Log("Grab action enabled");
+    }
+
+    public void OnDisable()
+    {
+        grabAction.Disable();
+        Debug.Log("Grab action disabled");
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"Trigger entered with: {other.gameObject.name}");
+        if(other.gameObject.GetComponent<Rigidbody>()) {
+            collidingObject = other.gameObject;
+            Debug.Log($"Colliding object set to: {collidingObject.name}");
+        } else {
+            Debug.Log("Object has no Rigidbody component");
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Trigger exit");
+        collidingObject = null;
+    }
+
+    public void GrabObject(InputAction.CallbackContext context)
+    {
+        Debug.Log("Grab action performed");
+        if (collidingObject != null) {
+            Debug.Log($"Attempting to grab: {collidingObject.name}");
+            objectInHand = collidingObject;
+            objectInHand.transform.SetParent(this.transform);
+            objectInHand.GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("Grab successful");
+        } else {
+            Debug.Log("No object to grab");
+        }
+    }
+
+    public void ReleaseObject(InputAction.CallbackContext context)
+    {
+        Debug.Log("Release action performed");
+        if (objectInHand != null) {
+            objectInHand.GetComponent<Rigidbody>().isKinematic = false;
+            objectInHand.transform.SetParent(null);
+            objectInHand = null;
+            Debug.Log("Object released");
+        }
+    }
+}
