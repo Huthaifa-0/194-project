e1296d4535111cae2522e329cb246cfa657f8ce7
elyseq <equigley@macalester.edu>
2024-12-09 17:18:43 -0600

    deleting things we didn't need
    Irene and I modified many scenes. We renamed Elma's scenes, deleted Elyse's scenes, and created LoadNextScene Script.

---
+++ Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadNextScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public class LoadUnderwater() {
+        SceneManager.LoadScene("WaterScene"); //change to match scene
+    }
+
+    public class LoadBeach() {
+        SceneManager.LoadScene("WaterScene"); //change to match scene
+    }
+}
