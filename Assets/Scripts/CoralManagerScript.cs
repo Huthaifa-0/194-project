ca43b89921107fd8f1b5ceb68222c51c9699e1e1
emostic <emostic@macalester.edu>
2024-12-16 22:23:54 -0600

    commenting out useless parts of script

--- Assets/Scripts/CoralManagerScript.cs
+++ Assets/Scripts/CoralManagerScript.cs
@@ -1,46 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
 
-public class Coralmanager : MonoBehaviour
-{
-    private float maxHealth;
-    private float currentHealth;
-    private Color color;
-    private float alpha;
-    void Start()
-    {
-        currentHealth=0; //initial health score is 0
-        alpha=1f;
-        color.a=alpha; //initialize renderer as fully opaque
-        UpdateMaterialTransparency(alpha);
-    }
+// public class Coralmanager : MonoBehaviour
+// {
+//     private float maxHealth;
+//     private float currentHealth;
+//     private Color color;
+//     private float alpha;
+//     void Start()
+//     {
+//         currentHealth=0; //initial health score is 0
+//         alpha=1f;
+//         color.a=alpha; //initialize renderer as fully opaque
+//         UpdateMaterialTransparency(alpha);
+//     }
 
-    // Update is called once per frame
-    void Update(){ 
+//     // Update is called once per frame
+//     void Update(){ 
 
-    }
+//     }
 
-    private void UpdateMaterialTransparency(float alphaUpdated){
-        currentHealth++;
-        // How would we take the health score from another class . can we make it public static to access it 
-        alpha= 1f - currentHealth/ maxHealth;
+//     private void UpdateMaterialTransparency(float alphaUpdated){
+//         currentHealth++;
+//         // How would we take the health score from another class . can we make it public static to access it 
+//         alpha= 1f - currentHealth/ maxHealth;
 
-        //method takes new alpha variable and applies it to all coral objects
-        //optimize by updating trasparency only when health score changes
-        //updating every frame can make it laggy
-        foreach (Transform child in transform){
-            Renderer coralRenderer = child.GetComponent<Renderer>();
-            if (coralRenderer != null){
-                Material overlayMaterial = coralRenderer.materials[1];
-                Color color = overlayMaterial.color;
-                color.a = alpha; // Update transparency
-                overlayMaterial.color = color;
-            }
+//         //method takes new alpha variable and applies it to all coral objects
+//         //optimize by updating trasparency only when health score changes
+//         //updating every frame can make it laggy
+//         foreach (Transform child in transform){
+//             Renderer coralRenderer = child.GetComponent<Renderer>();
+//             if (coralRenderer != null){
+//                 Material overlayMaterial = coralRenderer.materials[1];
+//                 Color color = overlayMaterial.color;
+//                 color.a = alpha; // Update transparency
+//                 overlayMaterial.color = color;
+//             }
             
                 
                 
-        }
+//         }
         
-    }
-}
+//     }
+// }
